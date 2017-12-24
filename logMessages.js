86fbdac913917d74023bb6bc47f9614c646a404c
srinskit <srinagrao2007@gmail.com>
2017-12-24 10:21:10 +0530

    Moving all log messages to single file

---
+++ logMessages.js
@@ -0,0 +1,6 @@
+/*jslint node: true */
+"use strict";
+module.exports = {
+    dbConnectionError: "[%s] Couldn't connect to DB at %s",
+    dbNoServerConfig: "[%s] Couldn't find any server config"
+};
